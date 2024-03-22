d6300a980f3b6e11bf7e27fc3c25bd1c171f559d
shingostar <shingostar@shore.ocn.ne.jp>
2024-03-22 23:17:40 +0900

    再提出L34_個人開発演習：従業員更新画面の実装_従業員更新画面が実装できたら、その内容をメンターにチェックしてもらいましょう

--- src/main/java/com/techacademy/constants/ErrorMessage.java
+++ src/main/java/com/techacademy/constants/ErrorMessage.java
@@ -32,6 +32,7 @@ public class ErrorMessage {
                     new ArrayList<String>(Arrays.asList("deleteError", "ログイン中の従業員を削除することは出来ません")));
             // 同一日付チェック用エラーメッセージ
             put(ErrorKinds.DATECHECK_ERROR, new ArrayList<String>(Arrays.asList("reportDateError", "既に登録されている日付です")));
+
         }
     };
 
